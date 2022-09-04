147b712688de6f0a1acb05eb9ca1210fa7adcc22
Or <o.parnassa@gmail.com>
2022-09-04 11:36:33 +0300

    added try/catch to API req

--- shopping-client/src/store/services/getChosenProductsService.ts
+++ shopping-client/src/store/services/getChosenProductsService.ts
@@ -1,6 +1,10 @@
 import axiosInstance from "./constants";
 
 export default async function getChosenProductsApi(chosenSmartphone: string) {
-  const result = await axiosInstance.get(`/search?q=${chosenSmartphone}`);
-  return result.data.products;
+  try {
+    const result = await axiosInstance.get(`/search?q=${chosenSmartphone}`);
+    return result.data.products;
+  } catch (error) {
+    alert("Oops! something went wrong")
+  }
 }
--- shopping-client/src/store/services/getProductsService.ts
+++ shopping-client/src/store/services/getProductsService.ts
@@ -1,8 +1,12 @@
 import axiosInstance from "./constants";
 
 export default async function getProductsApi() {
-  const result = await axiosInstance.get("/category/smartphones");  
-  return result.data.products;
+  try {
+    const result = await axiosInstance.get("/category/smartphones");
+    return result.data.products;
+  } catch (error) {
+    alert("Oops! something went wrong")
+  }
 }
 
 
