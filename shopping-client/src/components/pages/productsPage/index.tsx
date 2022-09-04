b4e4e6f49229140af07e5b1b734680e1180c23b6
Or <o.parnassa@gmail.com>
2022-09-04 10:47:33 +0300

    deleted unecessary files

--- shopping-client/src/components/pages/productsPage/index.tsx
+++ shopping-client/src/components/pages/productsPage/index.tsx
@@ -7,17 +7,17 @@ import getProducts from "../../../store/async/getProductsAction";
 import getChosenProducts from "../../../store/async/getChosenProductsAction";
 
 export default function ProductsPage() {
-  const products = useAppSelector((state) => state.products.products);
+  const products: any = useAppSelector((state) => state.products.products);
   let [currentDisplay, setCurrentDisplay] = useState(products);
 
   useEffect(() => {
     async function fetchData() {
       const response = await getProducts();
-      setCurrentDisplay(response);      
+      setCurrentDisplay(response);
     }
     fetchData();
-  }, []); 
-  
+  }, []);
+
   async function OrderBy(sortBy: any) {
     const newProductArray = ([...products].sort((a, b) =>
       sortBy < 2 ? a.price - b.price : sortBy === 2 ? b.price - a.price : a.rating - b.rating));
@@ -30,28 +30,28 @@ export default function ProductsPage() {
   }
 
   return (
-    <div className="container" style={{background:"#E0E0E0"}}>
-    <div style={{width: "85%", textAlign: "center", margin: "0 0", display: "inline-block",background:"#ffffff"}}>
-      <div className="searchDivsWrapper" style={{ display: "inline-block",width:"100%"}}>
-      <h1 style={{ textAlign: "left", marginLeft: "20px" }}> The Smartphone Shop  </h1>
-      <div className="searchDivs" style={{ display: "inline-block", margin: "auto", width: "30%",marginTop:"2%",marginBottom:"5%",textAlign:"center" }}>
-        <span>
-        <SearchByName
-          OrderByFreeSearch={OrderByFreeSearch}
-          options={products}
-        />
-        <br />
-        <SearchByCategory OrderBy={OrderBy} />
-        </span>
-      </div>
-      </div>
-      {currentDisplay.map((p) => {
-        return <div style={{ display: "inline-block", maxWidth: "340px", margin: "auto", marginRight: "20px" }}>
-          <ProductCard key={p.id} {...p}
-          />
+    <div className="container" style={{ background: "#E0E0E0" }}>
+      <div style={{ width: "85%", textAlign: "center", margin: "0 0", display: "inline-block", background: "#ffffff" }}>
+        <div className="searchDivsWrapper" style={{ display: "inline-block", width: "100%" }}>
+          <h1 style={{ textAlign: "left", marginLeft: "20px" }}> The Smartphone Shop  </h1>
+          <div className="searchDivs" style={{ display: "inline-block", margin: "auto", width: "30%", marginTop: "2%", marginBottom: "5%", textAlign: "center" }}>
+            <span>
+              <SearchByName
+                OrderByFreeSearch={OrderByFreeSearch}
+                options={products}
+              />
+              <br />
+              <SearchByCategory OrderBy={OrderBy} />
+            </span>
+          </div>
         </div>
-      })}
-    </div>
+        {currentDisplay.map((productIteration: any): any => {
+          return <div style={{ display: "inline-block", maxWidth: "340px", margin: "auto", marginRight: "20px" }}>
+            <ProductCard key={productIteration.id} {...productIteration}
+            />
+          </div>
+        })}
+      </div>
     </div>
   );
 }
--- shopping-client/src/store/hooks.ts
+++ shopping-client/src/store/hooks.ts
@@ -4,5 +4,3 @@ import { RootState, AppDispatch } from "./";
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-// in component use the useAppSelector method to get the stated selector
-// const state=  useAppSelector(state=>state.settings)
