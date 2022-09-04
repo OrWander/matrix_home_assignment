fbdf4563fe947544b78d8c8ac1c42afdc104baac
Or <o.parnassa@gmail.com>
2022-09-04 10:54:19 +0300

    types added

--- shopping-client/src/components/pages/productsPage/searchByName/index.tsx
+++ shopping-client/src/components/pages/productsPage/searchByName/index.tsx
@@ -1,20 +1,19 @@
 import { Autocomplete, TextField } from '@mui/material';
-import { useState } from 'react';
 
 export default function SearchByName(props: {
   options: Array<any>;
   OrderByFreeSearch:FunctionStringCallback;
 }) {
   
-  const updateValue:any = (newValue:string) => {
-    props.OrderByFreeSearch(newValue);
+  const updateSearchByName:Function = (chosenProduct:string) => {
+    props.OrderByFreeSearch(chosenProduct);
   };
   
   return (
     <div className="container">
       <Autocomplete
-        onChange={(event: any): any => updateValue(event.target.innerText)}
-        options={props.options.map((p) => { return p.title })}
+        onChange={(event: any): any => updateSearchByName(event.target.innerText)}
+        options={props.options.map((product) => { return product.title })}
         renderInput={(params) => <TextField {...params} label="Search by name" />}
       />
     </div>
